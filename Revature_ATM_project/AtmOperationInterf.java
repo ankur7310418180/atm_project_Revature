fbbe5de96007db1388b28c9f6d3d767ab7b957cd
Ankur Singh <62100752+ankur7310418180@users.noreply.github.com>
2022-08-06 01:38:32 +0530

    Add files via upload

---
+++ Revature_ATM_project/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package com.atm_project_revature;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
